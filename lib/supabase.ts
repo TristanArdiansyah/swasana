efb845f8be763591f1fef0beb75cff2c2f468b29
Tristan Ardiansyah <nandatristan@gmail.com>
2025-07-15 03:59:53 +0700

    bypass env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,4 +4,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl || "", supabaseAnonKey || "");
+export const supabase = createClient(supabaseUrl || "https://ukcxwccpzpjnrskjwifd.supabase.co", supabaseAnonKey || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrY3h3Y2NwenBqbnJza2p3aWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyMzE4NzgsImV4cCI6MjA1NzgwNzg3OH0._-wIwBA8lmTGU37NECqkcF6TQxb_ycEfMGhqhk1W-Kg");
